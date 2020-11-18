a330a284ff7b1b1864a689d62e36c207cceae15d
Riikka Korolainen <riikka.korolainen@outlook.com>
2020-11-18 17:18:50 +0200

    uibuild

--- frontend/build/service-worker.js
+++ frontend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.fd1fe38355e771a80c1e9fd9e97e73cb.js"
+  "/precache-manifest.7c5da385cfb464f5dcc64e1a5415849e.js"
 );
 
 self.addEventListener('message', (event) => {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,6 @@ import UnauthorizedView from './components/UnauthorizedView'
 import calendarService from './services/calendarService'
 
 const App = () => {
-  console.log('rendering')
   const [calendarEntries, setCalendarEntries] = useState([])
   const today = new Date(Date.now())
   const [authorized, setAuthorized] = useState(false)
